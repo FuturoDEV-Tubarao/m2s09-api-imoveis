deaba69b43b63aa527786b0b9601502d83066252
tiagoamp <tiagoamp@gmail.com>
2024-04-26 20:16:13 -0300

    Implementa servicos na API

---
+++ src/main/java/com/senai/imobiliaria/exception/RegistroExistenteException.java
@@ -0,0 +1,9 @@
+package com.senai.imobiliaria.exception;
+
+public class RegistroExistenteException extends RuntimeException {
+
+    public RegistroExistenteException(String mensagem) {
+        super(mensagem);
+    }
+    
+}
